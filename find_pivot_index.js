5206f20617ea654e00cd638543690cd0a1d5df61
Yogesh datta telange <48433068+Yogesh-developer@users.noreply.github.com>
2023-01-16 20:03:12 +0530

    day 7

---
+++ find_pivot_index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let leftSum = 0;
+  let rightSum = nums.reduce((a, b) => a + b);
+  for (let i = 0; i < nums.length; i++) {
+    rightSum = rightSum - nums[i];
+    if (rightSum == leftSum) return i;
+    leftSum = leftSum + nums[i];
+  }
+  return -1;
+};
